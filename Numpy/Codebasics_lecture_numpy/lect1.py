df0ef28615a90f03838d60708f4fe7cec7a5db84
Niketansinha1530 <niketansinha1@gmail.com>
2024-12-15 13:50:58 +0530

    Dimmension in Numpy array

---
+++ Numpy/Codebasics_lecture_numpy/lect1.py
@@ -0,0 +1,39 @@
+import sys
+import numpy as np
+
+
+
+# List V/s Numpy Array
+# numList = [1,2,3,4]
+# print("List: ",numList)
+
+
+# array= np.array([1,2,3,4])
+# print("Array : ",array)
+
+# listNum = []
+
+# for i in range(1,6):
+#     int_i= int(input("Enter Number: "))
+#     listNum.append(int_i)
+
+# listToArray = np.array(listNum)
+# print(listToArray)
+
+ar1 = np.array([1,2,3,4])
+ar2 = np.array([[1,2,3,4],[1,2,3,4]])
+ar3 = np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4],[1,2,3,4]]])
+
+
+print(ar1)
+print("ar1 dimension: ",ar1.ndim)
+print("******************************")
+print(ar2)
+print("ar2 dimension: ",ar2.ndim)
+print("******************************")
+print(ar3)
+print("ar3 dimension: ",ar3.ndim)
+
+newAR= np.array([1,2,3],ndmin=3)
+print(newAR)
+print(newAR.ndim)
