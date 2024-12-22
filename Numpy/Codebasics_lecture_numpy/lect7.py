c3baefcaf7501c18596100f42c3e50195b7846e8
Niketansinha1530 <niketansinha1@gmail.com>
2024-12-22 07:44:38 +0530

    More Arithmetic Function

--- Numpy/Codebasics_lecture_numpy/lect7.py
+++ Numpy/Codebasics_lecture_numpy/lect7.py
@@ -7,7 +7,7 @@ import numpy as np
 # np.sqrt(x)
 # np.sin(x)
 # np.cos(x)
-# np.cumsum(x)
+# np.cumsum(x) # Cumulative Sum
 
 
 
@@ -25,3 +25,14 @@ var2 = np.array([[21,22,3,44],[2,46,75,76]])
 print("Minimum along column " ,np.min(var2,axis=0))
 print("Minimum along row " ,np.min(var2,axis=1))
 print("Minimum in whole 2d array " ,np.min(var2))
+
+print("*******************************************")
+var3= np.arange(11) # return array from value 0 to n-1
+print(var3) #[ 0  1  2  3  4  5  6  7  8  9 10]
+var4 = np.cumsum(var3)# cum sum is sum of n number
+print(var4) #[ 0  1  3  6 10 15 21 28 36 45 55]
+
+print("*******************************************")
+
+numRoot = np.array([4,9,16,25,36,49])
+print("Square root of Array: ",np.sqrt(numRoot))
